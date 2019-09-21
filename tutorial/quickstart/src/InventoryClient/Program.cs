748112a85bc5c64d0f2aa4a7604cfa31b6978bd7
sebug <sebastian.gfeller@gmail.com>
2019-09-21 10:15:25 +0200

    Adding client

---
+++ tutorial/quickstart/src/InventoryClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InventoryClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
