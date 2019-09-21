4e6de1474a5c7f238f79b5ef0ae2eaf5df3855e9
sebug <sebastian.gfeller@gmail.com>
2019-09-21 10:11:21 +0200

    Adding inventory Api

---
+++ tutorial/quickstart/src/InventoryApi/Controllers/IdentityController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace InventoryApi.Controllers
+{
+    [Route("identity")]
+    [Authorize]
+    public class IdentityController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return new JsonResult(this.User.Claims.Select(
+                c => new { c.Type, c.Value }));
+        }
+    }
+}
